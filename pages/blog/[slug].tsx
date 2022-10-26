3b7617aee02540d7ca07886250a155390ca32cde
Josh Loesch <josh.loesch@strongholdim.com>
2022-10-25 21:09:01 -0500

    Make next global components work

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,23 +1,53 @@
 import { getMDXComponent } from "mdx-bundler/client";
-import { useMemo } from "react";
-import {
-  getPostData,
-  getAllPostSlugs,
-} from "../../lib/mdx";
+import { FC, useMemo } from "react";
+import styled from "styled-components";
+import { H1, H2, H3 } from "../../components/mdx/headers/headers";
+import { getPostData, getAllPostSlugs } from "../../lib/mdx";
+import Link from "next/link";
+import Image from "next/image";
+import { Router } from "next/router";
 
-const BlogPost = ({code, frontmatter}) => {
-  const Component = useMemo(() => getMDXComponent(code), [frontmatter]);
+interface BlogPostProps {
+  code: string;
+  frontmatter: {
+    title: string;
+    description: string;
+    date: string;
+  };
+}
+
+const components = {
+  h1: (props: any) => <H1 {...props} />,
+  h2: (props: any) => <H2 {...props} />,
+  h3: (props: any) => <H3 {...props} />,
+};
+
+const BlogPost: FC<BlogPostProps> = ({ code, frontmatter }) => {
+  const Component = useMemo(
+    () => getMDXComponent(code, { Link: Link, Image: Image, Router: Router }),
+    [code]
+  );
 
   return (
     <div>
       <h1>{frontmatter.title}</h1>
-      <Component />
+      <Div>
+        <Component components={components} />
+      </Div>
     </div>
   );
 };
 
 export default BlogPost;
 
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-inline: ${(props) =>
+    props.theme.spacing.blog.content.mobile.marginInline};
+`;
+
+// eslint-disable-next-line require-await
 export async function getStaticPaths() {
   const paths = getAllPostSlugs();
   console.log(paths);
@@ -27,7 +57,7 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params }: { params: { slug: string } }) {
   const postData = await getPostData(params.slug);
 
   console.log(postData);
